5a62938de565845a995df9536d37d9a55142e533
Fabricio C Zuardi <fabricio@fabricio.org>
2016-06-09 23:50:18 -0300

    use an ip address instead of localhost for the nginx config

--- tasks/makeconf.sh
+++ tasks/makeconf.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+ip=$1
 outputFolder="conf.d"
 templateFile="subdomain.conf"
 
@@ -8,9 +9,15 @@ mkdir -p "${outputFolder}"
 
 for domainWithPort in "$@"
 do
+    # skip parameters not in the format "domain:port"
+    if [[ "$domainWithPort" != *":"* ]]; then
+        continue
+    fi
     domain=$(echo ${domainWithPort} | cut -d ':' -f 1)
     port=$(echo ${domainWithPort} | cut -d ':' -f 2)
-    cat "${templateFile}" \
-        | sed -e "s/___DOMAIN___/"${domain}"/" -e "s/___PORT___/"${port}"/" \
+    cat "${templateFile}" | sed \
+        -e "s/___IPADDRESS___/"${ip}"/" \
+        -e "s/___DOMAIN___/"${domain}"/" \
+        -e "s/___PORT___/"${port}"/" \
         > "${outputFolder}/${domain}.conf"
 done
